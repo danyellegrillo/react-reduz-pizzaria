fb3a284a698abf6665555dea9692ca97b11ae628
DANYELLEGRILLO <dany_varre@hotmail.com>
2018-06-14 16:57:57 +0200

    project ready

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PizzaListContainer from './components/PizzaListContainer'
+import PizzaDetailsContainer from './components/PizzaDetailsContainer'
+import AddPizzaFormContainer from './components/AddPizzaFormContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PizzaListContainer/>
+        <PizzaDetailsContainer/>
+        <AddPizzaFormContainer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PizzaListContainer.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import PizzaList from './PizzaList'
+import { connect } from 'react-redux'
+
+class PizzaListContainer extends React.PureComponent {
+  selectPizza = (id) => {
+    this.props.dispatch({
+      type: 'SELECT_PIZZA',
+      payload: id
+    })
+
+    console.log('selected pizza:', id)
+  }
+
+  render() {
+    return <PizzaList pizzas={this.props.pizzas} selectPizza={this.selectPizza} />
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    pizzas: state.pizzas
+  }
+}
+
+export default connect(mapStateToProps)(PizzaListContainer)
